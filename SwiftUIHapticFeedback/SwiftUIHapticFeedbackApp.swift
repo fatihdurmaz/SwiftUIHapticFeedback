54e496965086c572939cc0e806326f63b5272469
Fatih Durmaz <fatih.durmaz@outlook.com>
2024-01-26 00:10:04 +0300

    Initial Commit

---
+++ SwiftUIHapticFeedback/SwiftUIHapticFeedbackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHapticFeedbackApp.swift
+//  SwiftUIHapticFeedback
+//
+//  Created by Fatih Durmaz on 26.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHapticFeedbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
